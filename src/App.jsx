109816c40f9115f8394c52182168afff7c4d0df8
VIRENDRA-18 <virusingh0125@gmail.com>
2026-02-14 01:09:19 +0530

    make responsive

--- src/App.jsx
+++ src/App.jsx
@@ -5,41 +5,47 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Properties from "./pages/Properties";
 import PropertyDetail from "./pages/PropertyDetail";
-
 import Agents from "./pages/Agents";
-
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
-
 
 function App() {
   const [searchText, setSearchText] = useState("");
 
   return (
-    <div className="min-h-screen">
+    <div className="flex flex-col min-h-screen">
+
+      {/* Navbar */}
       <Navbar
         searchText={searchText}
         setSearchText={setSearchText}
       />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
+      {/* Main Content */}
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
 
-        {/* 🔴 THIS LINE MUST BE EXACT */}
-        <Route
-          path="/properties"
-          element={<Properties searchText={searchText} />}
-        />
-        <Route path="/property/:id" element={<PropertyDetail />} />
+          <Route
+            path="/properties"
+            element={<Properties searchText={searchText} />}
+          />
 
-        <Route path="/agents" element={<Agents />} />
+          <Route
+            path="/property/:id"
+            element={<PropertyDetail />}
+          />
 
+          <Route path="/agents" element={<Agents />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
 
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      {/* Footer */}
       <Footer />
+
     </div>
   );
 }
